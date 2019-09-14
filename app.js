2a7f949c15805e93d884ae5e3f9b23d0029545a6
Pika1998 <prafzzraichurkar1@gmail.com>
2019-09-14 20:35:52 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.listen(port,()=>{
 //////////////////////////////////////// API CALLS ////////////////////////////////////////////////////////
 
 app.get('/',(req,res)=>{
-    res.redirect("/Student")
+    res.redirect("./public/index.html");
 })
 
 // Verification Calls (Common to almost all APIS)
