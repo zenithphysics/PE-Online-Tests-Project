c58008f15463fe1436d8f98951492b767a45d4a6
Pika1998 <prafzzraichurkar1@gmail.com>
2019-09-14 21:03:24 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.listen(port,()=>{
 //////////////////////////////////////// API CALLS ////////////////////////////////////////////////////////
 
 app.get('/',(req,res)=>{
-    res.redirect("https://photonecademy.com/tests/index.html");
+    res.redirect(__dirname+"/public/index.html");
 })
 
 // Verification Calls (Common to almost all APIS)
