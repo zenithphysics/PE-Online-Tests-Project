6c9e9f17e40c84c2201d6def135c75bed500d54d
Pika1998 <prafzzraichurkar1@gmail.com>
2019-09-17 20:43:51 +0530

    completed /modifyTest

--- app.js
+++ app.js
@@ -1248,7 +1248,6 @@ app.get('/admin',(req,res)=>{
                     {
                         console.log('\x1b[32m%s\x1b[1m', '[/modifyTest] - Modified Test Successfuly');  
                         res.json({is_verified:true,is_successful:true})
-
                     }
                 })
             }
