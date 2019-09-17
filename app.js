7cf1930587f129540878b5df4c926cb03b9d1c33
Pika1998 <prafzzraichurkar1@gmail.com>
2019-09-17 13:04:18 +0530

    working on modifying FST Test

--- app.js
+++ app.js
@@ -1171,22 +1171,22 @@ app.get('/admin',(req,res)=>{
                         // ReConvert buffer to base64 string for question black images
                         for(var i=0;i<test[0].question_images_black.length;i++)
                         {
-                            new_test[0].question_images_black[i] = new Buffer(test[0].question_images_black[i].data).toString('base64');
+                            new_test[0].question_images_black[i] = new Buffer(test[0].question_images_black[i]).toString('base64');
                         }
                         // Reconvert buffer to base64 string for question white images
                         for(var i=0;i<test[0].question_images_white.length;i++)
                         {
-                            new_test[0].question_images_white[i] = new Buffer(test[0].question_images_white[i].data).toString('base64');
+                            new_test[0].question_images_white[i] = new Buffer(test[0].question_images_white[i]).toString('base64');
                         }
                          // Reconvert buffer to base64 string for answer white images
                          for(var i=0;i<test[0].answer_images_white.length;i++)
                          {
-                             new_test[0].answer_images_white[i] = new Buffer(test[0].answer_images_white[i].data).toString('base64');
+                             new_test[0].answer_images_white[i] = new Buffer(test[0].answer_images_white[i]).toString('base64');
                          }
                            // ReConvert buffer to base64 string for answer black images
                         for(var i=0;i<test[0].answer_images_black.length;i++)
                         {
-                            new_test[0].answer_images_black[i] = new Buffer(test[0].answer_images_black[i].data).toString('base64');
+                            new_test[0].answer_images_black[i] = new Buffer(test[0].answer_images_black[i]).toString('base64');
                         }
                         res.json({is_verified:true,is_successful:true,test:new_test})
                     }
