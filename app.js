7ea7c281473a4b27aec52f3f44d4a8989a1acd54
Pika1998 <prafzzraichurkar1@gmail.com>
2019-09-19 14:11:36 +0530

    Making changes in /verifyStudent

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.get('/admin',(req,res)=>{
                 console.log('\x1b[32m%s\x1b[1m', '[/verifyStudent] - Student Verification Successful');  
                 console.log(`Student is ${authData.studentID}`)
                 var student = JSON.parse(JSON.stringify(authData));
-                Student.findById(student._id,(err,student=>{
+                Student.findOne(student.studentID,(err,student=>{
                     if(err || student==null)
                     {
                         console.log('\x1b[31m%s\x1b[1m', '[/verifyStudent] - Student Verification Failed');  
