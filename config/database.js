dc3e33036ea572c68a1fd21fff8618496851079b
Pika1998 <prafzzraichurkar1@gmail.com>
2019-09-26 00:34:20 +0530

    Changed MongoDB Connection string

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database:'mongodb://Prafulla:prafulla1998@photonecademy.com:27017/pe-tests-portal',
+    database:'mongodb://Prafulla:prafulla1998@165.22.214.215:27017/pe-tests-portal',
     secret:'yoursecret'
 }
