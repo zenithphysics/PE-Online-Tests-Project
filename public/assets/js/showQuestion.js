910e8d7662d479fd64dfc9403790ae162498621c
Pika1998 <prafzzraichurkar1@gmail.com>
2019-09-26 15:48:20 +0530

    working on dynamic question buttons

--- public/assets/js/showQuestion.js
+++ public/assets/js/showQuestion.js
@@ -55,7 +55,7 @@ $(function () {
         for (let index = 0; index <= test.questions.length; index++) {
             $(".questions").append(`<div class="question" style="border: 0px solid red;">
             <h4 class="p-2" style="border-bottom: 3px solid #25b5e9;">Question ` + index + ` : </h4>
-            <img class="w-50" src="${test.question_images_white[index]}" alt="">
+            <img class="w-50" src="data:image/jpeg;base64,${test.question_images_white[index]}" alt="">
             <table class="table table-borderless mt-2 p-5">
                 <tbody>
                     <tr>
