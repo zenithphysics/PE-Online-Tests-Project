776a0def311e1565ee2f402877c158390ce31ae1
Pika1998 <prafzzraichurkar1@gmail.com>
2019-09-26 16:42:11 +0530

    working on dynamic question buttons

--- public/assets/js/showQuestion.js
+++ public/assets/js/showQuestion.js
@@ -1,5 +1,5 @@
-$(function () {
-    
+
+if(test!=loading) {
     // alert("I'm ready")
     $(".toggleTop").click(function (e) {
         e.preventDefault();
@@ -257,4 +257,4 @@ $(function () {
             // alert(JSON.stringify(result))
         });
     });
-});
+}
